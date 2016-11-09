0030a89a8e2c48e90c89f069c16f20b6dda27b36
Vasco Curdia <vcurdia@gmail.com>
2016-11-08 17:08:59 -0800

    refine SpecPath setting

--- @DSGE/DSGE.m
+++ @DSGE/DSGE.m
@@ -15,6 +15,8 @@ classdef DSGE
         SpecPath = './';
         FileName = struct;
 %         PlotDir = struct;
+%        Options = struct;
+        TimeElapsed = struct;
         Param
         FixParam
         NumSolveParam
@@ -34,24 +36,31 @@ classdef DSGE
         NStateVar = 0;
         NShockVar = 0;
         NAuxVar = 0;
+        GensysAuthor = 'CS';
     end
    
     methods
         function obj = DSGE(Spec,SpecPath)
             if nargin>0
                 obj.Spec = Spec;
-            else
-                error('Need to specify DSGE Spec.')
             end
             if nargin<2
-                obj.SpecPath = [Spec,'/'];
+                if ~isempty(obj.Spec)
+                    obj.SpecPath = [Spec,'/'];
+                end
             end
-            mkdir(obj.SpecPath)
+        end
+        
+        function obj = set.SpecPath(obj,SpecPath)
+            if ~ismember(SpecPath,{'','./'})
+                obj.SpecPath = SpecPath;
+                mkdir(obj.SpecPath)
+            end
         end
         
         function obj = set.Param(obj,p)
             pType = 'Param';
-            [obj.(['N',pType]),obj.(pType)] = SetNames(obj,pType,p);
+            [obj.(['N',pType]),obj.(pType)] = SetNames(pType,p);
             if obj.NParam>0
                 obj.Param.PriorDist = p(:,2);
                 obj.Param.PriorMean = [p{:,3}]';
@@ -61,7 +70,7 @@ classdef DSGE
         
         function obj = set.FixParam(obj,p)
             pType = 'FixParam';
-            [obj.(['N',pType]),obj.(pType)] = SetNames(obj,pType,p);
+            [obj.(['N',pType]),obj.(pType)] = SetNames(pType,p);
             if obj.NFixParam>0
                 obj.FixParam.Values = [p{:,2}]';
             end
@@ -75,7 +84,7 @@ classdef DSGE
             eq = p{2};
             p = p{1};
             pType = 'NumSolveParam';
-            [obj.(['N',pType]),obj.(pType)] = SetNames(obj,pType,p);
+            [obj.(['N',pType]),obj.(pType)] = SetNames(pType,p);
             if obj.NNumSolveParam>0
                 obj.NumSolveParam.Guess = [p{:,2}]';
                 if isempty(eq) || (length(eq)<obj.NNumSolveParam)
@@ -88,7 +97,7 @@ classdef DSGE
     
         function obj = set.AuxParam(obj,p)
             pType = 'AuxParam';
-            [obj.(['N',pType]),obj.(pType)] = SetNames(obj,pType,p);
+            [obj.(['N',pType]),obj.(pType)] = SetNames(pType,p);
             if obj.NAuxParam>0
                 obj.AuxParam.Expressions = p(:,2);
             end
@@ -96,32 +105,42 @@ classdef DSGE
     
         function obj = set.ObsVar(obj,p)
             pType = 'ObsVar';
-            [obj.(['N',pType]),obj.(pType)] = SetNames(obj,pType,p);
+            [obj.(['N',pType]),obj.(pType)] = SetNames(pType,p);
         end
     
         function obj = set.StateVar(obj,p)
             pType = 'StateVar';
-            [obj.(['N',pType]),obj.(pType)] = SetNames(obj,pType,p);
+            [obj.(['N',pType]),obj.(pType)] = SetNames(pType,p);
         end
     
         function obj = set.ShockVar(obj,p)
             pType = 'ShockVar';
-            [obj.(['N',pType]),obj.(pType)] = SetNames(obj,pType,p);
+            [obj.(['N',pType]),obj.(pType)] = SetNames(pType,p);
         end
     
         function obj = set.AuxVar(obj,p)
             pType = 'AuxVar';
-            [obj.(['N',pType]),obj.(pType)] = SetNames(obj,pType,p);
+            [obj.(['N',pType]),obj.(pType)] = SetNames(pType,p);
             if obj.NAuxVar>0
                 obj.AuxEq = p(:,2);
             end
+        end
+        
+        function obj = set.ObsEq(obj,eq)
+            obj.ObsEq = eq;
+            CheckEq(obj,'Obs')
+        end
+    
+        function obj = set.StateEq(obj,eq)
+            obj.StateEq = eq;
+            CheckEq(obj,'State')
         end
     
     end
     
 end
 
-function [np,pp] = SetNames(obj,pType,p)
+function [np,pp] = SetNames(pType,p)
     if ~ismember(pType,{...
         'Param','FixParam','NumSolveParam','AuxParam',...
         'ObsVar','StateVar','ShockVar','AuxVar',...
@@ -141,3 +160,12 @@ function [np,pp] = SetNames(obj,pType,p)
     end
 end
         
+function CheckEq(obj,eqType)
+    nEq = length(obj.([eqType,'Eq']));
+    nVar = obj.(['N',eqType,'Var']);
+    if nEq~=nVar
+        error(['Number of %sEq (%i) does not match number of ' ...
+               'variables (%i).'],eqType,nEq,nVar)
+    end
+end
+
