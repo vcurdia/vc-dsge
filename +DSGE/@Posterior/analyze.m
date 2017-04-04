de65c58c5bf1dcce77f6fd35b768adad4b9a4354
Vasco Curdia <vcurdia@gmail.com>
2017-04-03 22:54:04 -0700

    complete post.analyze (need to test)

--- +DSGE/@Posterior/analyze.m
+++ +DSGE/@Posterior/analyze.m
@@ -25,9 +25,10 @@ op = updateoptions(op,varargin{:});
 
 sid = op.SampleID;
 sample = obj.Sample(sid);
-pN = obj.Model.Param.N;
+np = obj.Model.Param.N;
 pNames = obj.Model.Param.Names;
-auxN = obj.Model.AuxParam.N;
+nAux = obj.Model.AuxParam.N;
+auxNames = obj.Model.AuxParam.Names;
 
 fprintf('\n*** Analyzing MCMC Sample %.0f\n',sid)
 ttName = sprintf('AnalyzeSample%.0f',sid);
@@ -49,7 +50,7 @@ fprintf('Thinning used: %.0f\n', op.NThinning)
 fprintf('Total number of draws used: %.0f\n', nDrawsUsed)
 
 %% Generate AuxParam
-xdAux = zeros(auxN,nDrawsUsed);
+xdAux = zeros(nAux,nDrawsUsed);
 fh = @(x)obj.Model.mats(x);
 parfor jd=1:nDrawsUsed
     Matsj = fh(xd(:,jd));
@@ -96,9 +97,170 @@ else
     fprintf('Did not find MCMC draw with higher posterior density.\n')
     fprintf('Previous posterior mode kept!\n')
 end
+fprintf('\n')
 
-Mg likelihood next
+%% Marginal likelihood
+fprintf('Computing marginal likelihood\n')
+tau = 0.1:0.1:0.9;
+ntau = length(tau);
+pIdx = obj.EstimateIdx;
+npd = obj.NEstimate;
+% InvVar = inv(Post.Var);
+xdd = xd(pIdx,:)-repmat(obj.Mean(pIdx),1,nDrawsUsed);
+xddvar = xdd*xdd'/nDrawsUsed;
+[xddvaru,xddvars,xddvarv] = svd(xddvar);
+xddvarmd = min(size(xddvars));
+bigev = find(diag(xddvars(1:xddvarmd,1:xddvarmd))>1e-6);
+xddvardim = length(bigev);
+% [rank(xdvar),np,xdvardim]
+xddvarlndet = 0;
+for j=1:npd
+    if j>xddvardim
+        xddvars(j,j) = 0;
+    else
+        xddvarlndet = xddvarlndet+log(xddvars(j,j));
+        xddvars(j,j) = 1/xddvars(j,j);
+    end
+end
+InvVar = xddvaru*xddvars*xddvaru';
+lpdfMax = obj.LPDFMode;
+lpdfMean = mean(lpdfd);
+% [postMax,postMean]
+% Constant terms
+% lfConst = -log(tau)-np/2*log(2*pi)-1/2*log(det(Post.Var));
+lfConst = -log(tau)-npd/2*log(2*pi)-1/2*xddvarlndet;
+chi2Crit = chi2inv(tau,npd);
+% Calculate the ratio of f(x)/post(x)
+pw = zeros(ntau,nDrawsUsed);
+nB = 10;
+for jB=1:nB
+%     fprintf('Computing %.0f of %.0f...\n',jB,nB)
+    for jd=1:nDrawsUsed/nB
+        idx = nDrawsUsed/nB*(jB-1)+jd;
+        pratio = xdd(:,idx)'*InvVar*xdd(:,idx);
+        pw(:,idx) = (pratio<=chi2Crit).*...
+            exp(lfConst-1/2*pratio-lpdfd(idx)+lpdfMean);
+    end
+end
+% harmonic mean
+% LogMgLikelihood = postMean-log(mean(pw,2));
+LogMgLikelihood = zeros(ntau,1);
+for jtau=1:ntau
+  pwj = pw(jtau,:);
+  pwj = pwj(~isinf(pwj));
+  pwj = pwj(~isnan(pwj));
+  LogMgLikelihood(jtau) = lpdfMean-log(mean(pwj,2));
+end
+obj.LogMgLikelihood = LogMgLikelihood(tau==0.5);
+% obj.LogMgLikelihood = mean(LogMgLikelihood);
+for jt = 1:ntau
+    obj.Sample(sid).LogMgLikelihoodtau(jt).Tau = tau(jt);
+    obj.Sample(sid).LogMgLikelihoodtau(jt).Value = LogMgLikelihood(jt);
+end
 
+
+%% display results on screen
+fprintf('\nResults from MCMC sample analysis:')
+fprintf('\n==================================\n')
+pNames = obj.Model.Param.Names;
+pNameLength = [cellfun('length',pNames)];
+pNameLengthMax = max(pNameLength);
+DispList = {'','',pNames;
+            'Prior','Dist',obj.Prior.Dist;
+            '','   Mean',obj.Prior.Mean;
+            '','     SD',obj.Prior.SD;
+            '','     5%',obj.Prior.Prc05;
+            '',' Median',obj.Prior.Median;
+            '','    95%',obj.Prior.Prc95;
+            'Posterior','   Mode',obj.Mode;
+            '','   Mean',obj.Mean;
+            '','     SD',obj.SD;
+            '','     5%',obj.Prc05;
+            '',' Median',obj.Median;
+            '','    95%',obj.Prc95;
+           };
+nc = size(DispList,1);
+for jr=1:2
+    str2show = sprintf(['%-',int2str(pNameLengthMax),'s'],DispList{1,jr});
+    str2show = sprintf('%s  %-5s',str2show,DispList{2,jr});
+    for jc=3:nc
+        str2show = sprintf('%s  %-7s',str2show,DispList{jc,jr});
+    end
+    disp(str2show)
+end
+for jp=1:obj.Model.Param.N
+    str2show = sprintf(['%',int2str(pNameLengthMax),'s'],DispList{1,3}{jp});
+    str2show = sprintf('%s  %5s',str2show,DispList{2,3}{jp});
+    for jc=3:nc
+        str2show = sprintf('%s  %7.3f',str2show,DispList{jc,3}(jp));
+    end
+    disp(str2show)
+end
+fprintf('\n')
+
+auxN = obj.Model.AuxParam.N;
+auxNames = obj.Model.AuxParam.Names;
+auxNameLength = [cellfun('length',auxNames)];
+auxNameLengthMax = max(auxNameLength);
+DispList = {'','',auxNames;
+            'Prior','   Mean',obj.Prior.Sample.AuxParam.Mean;
+            '','     SD',obj.Prior.Sample.AuxParam.SD;
+            '','     5%',obj.Prior.Sample.AuxParam.Prc05;
+            '',' Median',obj.Prior.Sample.AuxParam.Median;
+            '','    95%',obj.Prior.Sample.AuxParam.Prc95;
+            'Posterior','   Mode',xAux;
+            '','   Mean',obj.Sample.AuxParam.Mean;
+            '','     SD',obj.Sample.AuxParam.SD;
+            '','     5%',obj.Sample.AuxParam.Prc05;
+            '',' Median',obj.Sample.AuxParam.Median;
+            '','    95%',obj.Sample.AuxParam.Prc95;
+           };
+nc = size(DispList,1);
+for jr=1:2
+    str2show = sprintf(['%-',int2str(pNameLengthMax),'s'],DispList{1,jr});
+    for jc=2:nc
+        str2show = sprintf('%s  %-7s',str2show,DispList{jc,jr});
+    end
+    disp(str2show)
+end
+for jp=1:auxN
+    str2show = sprintf(['%',int2str(auxNameLengthMax),'s'],DispList{1,3}{jp});
+    for jc=2:nc
+        str2show = sprintf('%s  %7.3f',str2show,DispList{jc,3}(jp));
+    end
+    disp(str2show)
+end
+fprintf('\n')
+
+% show mg Likelihood
+fprintf('\nMarginal likelihood:')
+fprintf('\n====================\n')
+for jt=1:ntau
+    fprintf('tau = %3.1f, log-marginal likelihood = %9.4f\n',...
+            tau(jt),LogMgLikelihood(jt))
+end
+disp(' ')
+
+% show correlation matrix
+fprintf('\nCorrelation matrix:')
+fprintf('\n===================\n')
+namelength = [cellfun('length',pNames)];
+namelengthmax = max(namelength);
+str2show = sprintf(['%',int2str(namelengthmax),'s'],'');
+for jc=1:np
+    str2show = sprintf(['%s  %',int2str(namelengthmax),'s'],str2show,...
+                       pNames{jc});
+end
+disp(str2show)
+for jr=1:np
+    str2show = sprintf(['%-',int2str(namelengthmax),'s'],pNames{jr});
+    for jc=1:np
+        str2show = sprintf(['%s  %',int2str(namelengthmax),'.4f'],str2show,...
+                           obj.Corr(jr,jc));
+    end
+    disp(str2show)
+end
+disp(' ')
 
 %% Finish up
 obj.TimeElapsed.stop(ttName)
