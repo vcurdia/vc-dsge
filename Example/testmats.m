682eb534cdc474a88e6a96618a14caecc441d981
Vasco Curdia <vcurdia@gmail.com>
2017-11-02 22:38:28 -0700

    complete proof of concept

--- Example/testmats.m
+++ Example/testmats.m
@@ -11,24 +11,52 @@ tt.start('Setup')
 
 %% simple example setup
 Param = {'a';'b';'c'};
-AuxParam = {'d','a+b'};
-
+AuxParam = {'d','4+a+b'};
+Var = {'x';'y';'z'};
+Eq = {...
+    'a*x-z';
+    '2+x*d+b*y';
+    '5-c*y';
+    };
 x0 = [1;2;3];
 
 %% generate mats
 AllParam = [Param;AuxParam(:,1)];
 vcsym(AllParam{:})
 
-mats.Param = @(x)x;
-
 nAuxParam = size(AuxParam,1);
-f.AuxParam = cell(nAuxParam,1);
+fAuxParam = cell(nAuxParam,1);
 for j=1:nAuxParam
     Auxj = eval(AuxParam{j,2});
-    f.AuxParam{j} = matlabFunction(Auxj,'Vars',Param);
+    fAuxParam{j} = matlabFunction(Auxj,'Vars',Param);
 end
-mats.AuxParam = @(x)buildmat(f.AuxParam,x,nAuxParam,1);
+f.AuxParam = @(x)buildmat(fAuxParam,x,nAuxParam,1);
+clear fAuxParam
 
+vcsym(Var{:})
+nVar = length(Var);
+var = sym(zeros(nVar,1));
+for j=1:nVar
+    var(j) = eval(Var{j});
+end
+nEq = length(Eq);
+eq = sym(zeros(nEq,1));
+for j=1:nEq
+    eq(j) = eval(Eq{j});
+end
+EqMat = jacobian(eq,var);
+fEq = cell(nEq*nVar,1);
+for j=1:nEq*nVar
+    fEq{j} = matlabFunction(EqMat(j),'Vars',AllParam);
+end
+f.Eq = @(x)buildmat(fEq,x,nEq,nVar);
+clear fEq
+
+%% Test functions
+mats.Param = x0;
+mats.AuxParam = f.AuxParam(x0);
+mats.AllParam = [mats.Param;mats.AuxParam];
+mats.Eq = f.Eq(mats.AllParam);
 
 %% Finish up
 tt.stop('Setup')
