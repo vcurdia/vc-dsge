e9630e95c347f3851e3cffb04f9e1dcb00128a63
Vasco Curdia <vcurdia@gmail.com>
2017-11-11 19:26:11 -0800

    update example files

--- Example/setupdsge.m
+++ Example/setupdsge.m
@@ -13,7 +13,7 @@
 %% Preamble
 clear all
 setpath
-% addpath('Mats/')
+addpath('Mats/')
 set(groot,'defaultTextInterpreter','latex'); % 
 set(groot,'defaultLegendInterpreter','latex');
 tt = TimeTracker;
@@ -23,7 +23,6 @@ tt.start('Setup')
 specName = 'MyDSGE';
 specPath = specName;
 basePath = '../';
-
 mkdir(specPath)
 cd(specPath)
 model = DSGE.Model(specName);
@@ -157,6 +156,7 @@ model.StateEq = {...
 
 %% Generate Mats
 model.genmats([basePath,'Mats/'])
+% model.genmats
 mats = model.mats(model.Param.Values);
 
 %% Describe Prior
@@ -164,6 +164,7 @@ prior = DSGE.Prior(model);
 
 %% Data
 data = DSGE.Data([basePath,'Data/Data_1987q3_2009q3.csv']);
+% data = DSGE.Data('Data_1987q3_2009q3.csv');
 data.Var = model.ObsVar.Names;
 op.Sim.Data = data;
 op.Sim.Tick.Labels = {'1990q1','1995q1','2000q1','2005q1'};
