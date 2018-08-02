6c7faac2892e47e879c62e3563a35ab90b4d5df4
Vasco Curdia <vcurdia@gmail.com>
2018-08-01 17:22:30 -0700

    minor tweak in model irf

--- +DSGE/@Model/irf.m
+++ +DSGE/@Model/irf.m
@@ -37,7 +37,7 @@ if isempty(op.ShockSize), op.ShockSize = ones(1,nShocks2Show); end
 
 %% Preamble
 
-fprintf('\nMaking IRF\n')
+fprintf('\n*** Making IRF\n')
 ttName = ['IRF',op.FNSuffix];
 obj.TimeTracker.start(ttName)
 
