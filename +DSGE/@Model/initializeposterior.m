4582ab9449dc6d28fecc58b891838258ea9ee091
Vasco Curdia <vcurdia@gmail.com>
2018-08-27 17:15:31 -0700

    add basic posterior initialization and methods

--- +DSGE/@Model/initializeposterior.m
+++ +DSGE/@Model/initializeposterior.m
@@ -13,6 +13,17 @@ function initializeposterior(obj)
 % Copyright 2018 by Vasco Curdia
 
 
-%% Preamble
 fprintf('Initializing posterior\n')
 
+obj.Posterior.EstimateIdx = ~ismember(obj.Prior.Dist,{'C'});
+obj.Posterior.NEstimate = sum(obj.Posterior.EstimateIdx);
+obj.Posterior.Mode = obj.Param.Values;
+obj.Posterior.ModeLPDF = obj.posteriorlpdf(obj.Posterior.Mode);
+fprintf('Posterior log-pdf using Param.Values is %0.4f.\n',...
+        obj.Posterior.ModeLPDF);
+obj.Posterior.Mean = obj.Prior.Mean;
+obj.Posterior.SD = obj.Prior.SD;
+obj.Posterior.Var = diag(obj.Prior.SD.^2);
+obj.Posterior.Median = obj.Prior.Median;
+obj.Posterior.Prc05 = obj.Prior.Prc05;
+obj.Posterior.Prc95 = obj.Prior.Prc95;
