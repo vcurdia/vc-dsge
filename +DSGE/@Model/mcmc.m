590f2ec6bcf3422db76a16c2416aad4643a53123
Vasco Curdia <vcurdia@gmail.com>
2018-08-27 18:06:42 -0700

    update mcmc

--- +DSGE/@Model/mcmc.m
+++ +DSGE/@Model/mcmc.m
@@ -33,39 +33,39 @@ if isempty(op.CalibrateMCMC)
 end
 if op.CalibrateMCMC
     op.JumpScale = obj.calibratemcmc(op);
-    save(sprintf('_%s_MCMC_%.0f_CalibrateJump',obj.Model.Name,obj.MCMCStage))
+    save(sprintf('_%s_MCMC_%.0f_CalibrateJump',obj.Name,obj.Post.MCMCStage))
 end
 
 %% Preparations
 
-if isempty(obj.MCMCStage), obj.MCMCStage = 1; end
-fprintf('\n*** Generating MCMC Sample %.0f\n',obj.MCMCStage)
-ttName = sprintf('MCMC%.0f',obj.MCMCStage);
+if isempty(obj.Post.MCMCStage), obj.Post.MCMCStage = 1; end
+fprintf('\n*** Generating MCMC Sample %.0f\n',obj.Post.MCMCStage)
+ttName = sprintf('MCMC%.0f',obj.Post.MCMCStage);
 obj.TimeTracker.start(ttName)
 
-pIdx = obj.EstimateIdx;
+pIdx = obj.Post.EstimateIdx;
 
-obj.MCMCSample.NChains = op.NChains;
-obj.MCMCSample.NDraws = op.NDraws;
+obj.Post.MCMCSample.NChains = op.NChains;
+obj.Post.MCMCSample.NDraws = op.NDraws;
 if ~op.Augment
-    obj.MCMCSample.JumpScale = op.JumpScale;
-    obj.MCMCSample.JumpVar = op.JumpScale^2/obj.NEstimate*obj.Var(pIdx,pIdx);
-    obj.MCMCSample.NRejections = zeros(1,op.NChains);
+    obj.Post.MCMCSample.JumpScale = op.JumpScale;
+    obj.Post.MCMCSample.JumpVar = op.JumpScale^2/obj.Post.NEstimate*obj.Post.Var(pIdx,pIdx);
+    obj.Post.MCMCSample.NRejections = zeros(1,op.NChains);
 end
-obj.MCMCSample.FileNameDraws = cell(op.NChains,1);
+obj.Post.MCMCSample.FileNameDraws = cell(op.NChains,1);
 for jChain=1:op.NChains
-    obj.MCMCSample.FileNameDraws{jChain} = sprintf(...
-        '%s_MCMC_%.0f_Chain_%.0f',obj.Model.Name,obj.MCMCStage,jChain);
+    obj.Post.MCMCSample.FileNameDraws{jChain} = sprintf(...
+        '%s_MCMC_%.0f_Chain_%.0f',obj.Name,obj.Post.MCMCStage,jChain);
 end
-obj.MCMCSample.FileNameRedux = [];
+obj.Post.MCMCSample.FileNameRedux = [];
 
 
 %% create MCMC chains
 [npx0,nx0] = size(op.x0);
 x0 = cell(1,4);
 if nx0>0
-    if npx0==obj.Model.Param.N
-        op.x0 = op.x0(obj.EstimateIdx,:); 
+    if npx0==obj.Param.N
+        op.x0 = op.x0(obj.Post.EstimateIdx,:); 
     end
     for j=1:nx0
         x0{j} = op.x0(:,j);
@@ -73,17 +73,17 @@ if nx0>0
 end
 opChain.Augment = op.Augment;
 opChain.NDraws = op.NDraws;
-opChain.JumpVar = obj.MCMCSample.JumpVar;
-nRejections = obj.MCMCSample.NRejections;
+opChain.JumpVar = obj.Post.MCMCSample.JumpVar;
+nRejections = obj.Post.MCMCSample.NRejections;
 parfor jChain=1:op.NChains
     opj = opChain;
-    opj.fn = obj.MCMCSample.FileNameDraws{jChain}
+    opj.fn = obj.Post.MCMCSample.FileNameDraws{jChain}
     opj.x0 = x0{jChain};
     nRejections(jChain) = obj.mcmcchain(opj);
 end
     
 %% show rejection rates
-obj.MCMCSample.NRejections = nRejections;
+obj.Post.MCMCSample.NRejections = nRejections;
 for jChain=1:op.NChains
     fprintf('Chain %.0f: JumpScale = %4.2f, Rejection rate = %5.1f%%\n',...
             jChain,op.JumpScale,nRejections(jChain)/op.NDraws*100)
@@ -91,12 +91,12 @@ end
 fprintf('\n')
 
 %% save workspace
-save(sprintf('_%s_MCMC_%.0f',obj.Model.Name,obj.MCMCStage))
+save(sprintf('_%s_MCMC_%.0f',obj.Name,obj.Post.MCMCStage))
 
 %% Clean up
 if ~op.KeepLogs
     for jChain=1:op.NChains
-        delete(sprintf('%s.log',obj.MCMCSample.FileNameDraws{jChain},jChain));
+        delete(sprintf('%s.log',obj.Post.MCMCSample.FileNameDraws{jChain},jChain));
     end
 end
 
@@ -104,8 +104,8 @@ end
 obj.TimeTracker.stop(ttName)
 
 %% Run MCMC analysis
-if op.AnalyzeParam, obj.analyzeparam, end
-if op.AnalyzeConvergence, obj.analyzeconvergence, end
+if op.AnalyzeParam, obj.analyzepost, end
+if op.AnalyzeConvergence, obj.mcmcconvergence, end
 if op.MCMCRedux, obj.mcmcredux, end
 
 
