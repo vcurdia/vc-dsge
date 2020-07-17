75606113fa133f716a907ed5b783f4a5486bdcec
Vasco Curdia <vcurdia@gmail.com>
2020-07-16 18:36:37 -0700

    set fn names

--- +DSGE/@MCMC/MCMC.m
+++ +DSGE/@MCMC/MCMC.m
@@ -28,6 +28,7 @@ classdef MCMC < matlab.mixin.Copyable
             if nargin>0
                 obj.model = model;
                 obj.jumpvar = model.Post.Var;
+                obj.setfn;
             end
         end
         
@@ -38,6 +39,18 @@ classdef MCMC < matlab.mixin.Copyable
         
         function set.ndrawskeep(obj,n)
             obj.ndrawskeep = min(n,obj.ndraws);
+        end
+        
+        function set.nchains(obj,n)
+            obj.nchains = n;
+            obj.setfn;
+        end
+        
+        function setfn(obj)
+            obj.fnsample = sprintf('%s-mcmc-sample',obj.model.Name);
+            for j=1:obj.nchains
+                obj.fnchains{j} = sprintf('%s-mcmc-chain-%.0f',obj.model.Name,j);
+            end
         end
         
         
